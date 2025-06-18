7e3b68d4315c29ba8cc0936b8c5b98416da4be1c
Jamieel-L <jayjuelz7@gmail.com>
2025-06-18 17:30:38 -0400

    Add files via upload

---
+++ Mountain_Array.py
@@ -0,0 +1,18 @@
+class Mountain_Array(object):
+    def __init__ (self,arr):
+        self.arr = arr
+    def peakIndexinMountainArr(self):
+        left,right = 0, len(self.arr)-1 #start indexing at 0
+        while left < right:  #create logic for right being greater than left
+            mid = (left+right)// 2
+            if self.arr[mid]<self.arr[mid+1]:  #start ascension, moves right
+                left = mid + 1
+            else:                   #start decension moves left
+                right = mid
+        return left
+mountain = Mountain_Array([0,5,10,2])  #checks peak index and returns it
+print(mountain.peakIndexinMountainArr())
+
+import play 
+
+print(play.percentcomp(110,40))
